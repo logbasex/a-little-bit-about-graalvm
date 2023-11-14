124144bce002242a468da6015a91ba71e264e4f8
logbasex <contact.hoducanh@gmail.com>
2023-11-14 09:25:37 +0700

    add API doc.

--- polyglot/src/main/java/com/logbasex/controller/MLController.java
+++ polyglot/src/main/java/com/logbasex/controller/MLController.java
@@ -13,6 +13,17 @@ import java.io.IOException;
 public class MLController {
 	private final MachineLearningService mlSv;
 	
+	/**
+	 * curl --location --request POST 'http://localhost:8080/predict' \
+	 * --header 'Content-Type: application/json' \
+	 * --data-raw '[
+	 *     12.5,
+	 *     15.3,
+	 *     22.8,
+	 *     9.1,
+	 *     17.6
+	 * ]'
+	 */
 	@PostMapping("/predict")
 	public double predict(@RequestBody double[] inputData) throws IOException {
 		return mlSv.runModel(inputData);
